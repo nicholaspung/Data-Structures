5c7ae2dc1699ad3bd0218215528dcd4af1cc5137
Nicholas Pung <nckpng95@gmail.com>
2019-10-14 13:59:56 -0700

    Finished up rest of methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -93,12 +93,24 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+        self.add_to_head(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+        self.add_to_tail(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
