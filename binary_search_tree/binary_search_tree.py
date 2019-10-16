11b8d448988ec4650277772ad960ef1c91b2773b
Nicholas Pung <nckpng95@gmail.com>
2019-10-16 12:29:47 -0700

    mvp complete, finished BST for_each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -49,7 +49,11 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        cb(value)
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
